162c15efb8b706ba2bab7fd19f20d31f8e2115ed
Hugo Burton <burto732@gmail.com>
2024-02-29 12:17:51 +1000

    Updated print function made notes on data types

--- Week 2/data_loading.py
+++ Week 2/data_loading.py
@@ -8,7 +8,7 @@ import os
 
 def load_data(file_path: str)-> pd.DataFrame:
     df = None
-    
+
     # If Excel file
     if file_path.endswith(".xlsx"):
         df = pd.read_excel(file_path)
@@ -21,13 +21,18 @@ def load_data(file_path: str)-> pd.DataFrame:
     return df
 
 
-def display_data(data: pd.DataFrame):
+def display_data(data: pd.DataFrame, print_metadata: bool = False):
+    if print_metadata:
+        print("Metadata")
+        for column in data.columns:
+            print(f"Column: {column}")
+            print(f"Data type: {data[column].dtype}")
+            print(f"Number of null values: {data[column].isnull().sum()}")
+            print(f"Number of unique values: {data[column].nunique()}")
+
+    print("Printing Data")
     for column in data.columns:
-        print(f"Column: {column}")
-        print(f"Data type: {data[column].dtype}")
-        print(f"Number of missing values: {data[column].isnull().sum()}")
-        print(f"Number of unique values: {data[column].nunique()}")
-        print(f"Unique values: {data[column].unique()}")
+        print(f"Values: \n{data[column]}")
         print("\n")
 
 
@@ -41,10 +46,15 @@ def main():
 
     pokemon_data = load_data(pokemon_data_file)
     display_data(pokemon_data)
+    # Data is partly text and mostly numerical floats
+
     seedlings_data = load_data(seedlings_data_file)
     display_data(seedlings_data)
+    # Data is mostly numerical with some text
+
     snakes_data = load_data(snakes_data_file)
     display_data(snakes_data)
+    # Data is a mix of numerical, categorical and text based.
 
 
 if __name__ == "__main__":
