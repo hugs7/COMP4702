6d7985a321a44777f5af154a157276b1be1ab990
Hugo Burton <burto732@gmail.com>
2024-02-29 13:03:37 +1000

    Improved code to make loading and processing into a function

--- Week 2/p1_loading.py
+++ Week 2/p1_loading.py
@@ -5,47 +5,56 @@ Part 1: Data Loading and Pre-processing
 
 import os
 from colorama import Fore, Style
+import pandas as pd
 
 import load_data
 import display_data
 import replace_values
 import encode_data
 
+def check_file_exists(file_path: str) -> bool:
+    file_exists = os.path.exists(file_path)
+    if not file_exists:
+        raise FileNotFoundError(f"File {file_path} not found")
+
+def load_and_process_data(data_file: str, replace_null: bool = False) -> pd.DataFrame:
+    check_file_exists(data_file)
+
+    # Load Data
+    print(f"{Fore.LIGHTBLUE_EX}Loading Data{Style.RESET_ALL}")
+    data = load_data.load_data(data_file)
+    print(f"Data loaded from {data_file}")
+
+    if replace_null:
+        # Replace null values with mean instead
+        print(f"{Fore.LIGHTBLUE_EX}Replacing Null Values with Mean{Style.RESET_ALL}")
+        data = replace_values.replace_null_values_with_mean(data)
+    else:
+        # Remove null values
+        print(f"{Fore.LIGHTBLUE_EX}Removing Null Values{Style.RESET_ALL}")
+        data = replace_values.remove_null_values(data)
+    
+    # Encode non-numeric data
+    print(f"{Fore.LIGHTBLUE_EX}Encoding Non-Numeric Data{Style.RESET_ALL}")
+    data_encoded = encode_data.encode_non_numeric_data(data)
+
+    return data_encoded
+
 
 def main():
     current_folder = os.path.dirname(os.path.abspath(__file__))
+    data_folder = os.path.join(current_folder, "data")
 
-    pokemon_data_file = os.path.join(current_folder, "pokemonsrt.csv")
-    seedlings_data_file = os.path.join(current_folder, "seedlings.csv")
-    snakes_data_file = os.path.join(current_folder, "Snakes.xlsx")
+    pokemon_data_file = os.path.join(data_folder, "pokemonsrt.csv")
+    seedlings_data_file = os.path.join(data_folder, "seedlings.csv")
+    snakes_data_file = os.path.join(data_folder, "Snakes.xlsx")
 
     data_files = [pokemon_data_file, seedlings_data_file, snakes_data_file]
 
     for data_file in data_files:
-        print(f"{Fore.LIGHTGREEN_EX}Data File: {Fore.MAGENTA}{data_file}{Style.RESET_ALL}")
-        # Check if file exists
-        if not os.path.exists(data_file):
-            raise FileNotFoundError(f"{Fore.RED}File {data_file} not found{Fore.WHITE}")
+        data_processed = load_and_process_data(data_file, replace_null=True)
         
-        # Load Data
-        print(f"{Fore.LIGHTBLUE_EX}Loading Data{Style.RESET_ALL}")
-        data = load_data.load_data(data_file)
-        print(f"Data loaded from {data_file}")
-
-        # Remove null values
-        print(f"{Fore.LIGHTBLUE_EX}Removing Null Values{Style.RESET_ALL}")
-        data_null_removed = replace_values.remove_null_values(data)
-        display_data.display_data(data_null_removed, print_metadata=False, head_only = True)
-
-        # Replace null values with mean instead
-        print(f"{Fore.LIGHTBLUE_EX}Replacing Null Values with Mean{Style.RESET_ALL}")
-        data_null_replaces = replace_values.replace_null_values_with_mean(data)
-        display_data.display_data(data_null_replaces, print_metadata=False, head_only = True)
-
-        # Encode non-numeric data
-        print(f"{Fore.LIGHTBLUE_EX}Encoding Non-Numeric Data{Style.RESET_ALL}")
-        data_encoded = encode_data.encode_non_numeric_data(data_null_replaces)
-        display_data.display_data(data_encoded, print_metadata=False, head_only = True)
+        display_data.display_data(data_processed, print_metadata=False, head_only = True)
 
         print("-"*100)
 
