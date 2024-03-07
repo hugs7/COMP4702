4c87282d1ac540323e9569758bba55d81be2aeb7
Hugo Burton <burto732@gmail.com>
2024-03-07 13:23:25 +1000

    Updated scatter plot functoin Made scatter plot for 2d data

--- Week 3/scatterplot.py
+++ Week 3/scatterplot.py
@@ -8,17 +8,55 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def scatterplot(data):
-    fig = plt.figure()
+def scatterplot_2d(data) -> None:
+    """
+    Create a scatter plot with colored data points for 2D data.
+
+    Parameters:
+    - data (pandas.DataFrame): The input data containing the columns for x, y, and color values.
+
+    Returns:
+    None
+    """
+
+    fig, ax = plt.subplots()
     colours = ListedColormap(["r", "b"])
-    scatter = plt.scatter(
-        data.loc[:, "X1"],
-        data.loc[:, "X2"],
-        c=data.loc[:, "Y"],
-        cmap=colours,
-        label=data.loc[:, "Y"],
+    scatter = ax.scatter(
+        data.iloc[:, 0],
+        data.iloc[:, 1],
     )
-    plt.legend(*scatter.legend_elements())
-    plt.ylabel("X2")
-    plt.xlabel("X1")
+    ax.legend(*scatter.legend_elements())
+    ax.set_ylabel("Y")
+    ax.set_xlabel("X")
+    plt.show()
+
+
+def scatterplot_with_colour(
+    data, x_column: str, y_column: str, color_column: str
+) -> None:
+    """
+    Create a scatter plot with colored data points.
+
+    Parameters:
+    - data (pandas.DataFrame): The input data containing the columns for x, y, and color values.
+    - x_column (str): The name of the column in the data frame to be used as the x-axis values.
+    - y_column (str): The name of the column in the data frame to be used as the y-axis values.
+    - color_column (str): The name of the column in the data frame to be used as the color values.
+
+    Returns:
+    None
+    """
+
+    fig, ax = plt.subplots()
+    colours = ListedColormap(["r", "b"])
+    scatter = ax.scatter(
+        data[x_column],
+        data[y_column],
+        c=data[color_column],
+        cmap=colours,
+        label=data[color_column],
+    )
+    ax.legend(*scatter.legend_elements())
+    ax.set_ylabel(y_column)
+    ax.set_xlabel(x_column)
     plt.show()
