fe3abefd2edd7f32830fb0bfab8026f07bb2b416
Hugo Burton <burto732@gmail.com>
2024-03-07 12:35:41 +1000

    Implemented knn classifier

--- Week 3/knn.py
+++ Week 3/knn.py
@@ -3,6 +3,8 @@ Applies knn classifier to data
 
 """
 
+from colorama import Fore
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
@@ -40,5 +42,38 @@ def test_train_split(
     return X_train, X_test, y_train, y_test
 
 
-def knn():
-    return 0
+def knn(
+    X_train: DataFrame,
+    X_test: DataFrame,
+    y_train: DataFrame,
+    y_test: DataFrame,
+    k: int = 3,
+) -> tuple[np.ndarray, float, float]:
+    """
+    Performs k-nearest neighbors classification on the given training and test data.
+
+    Parameters:
+        X_train (DataFrame): The feature matrix of the training data.
+        y_train (DataFrame): The target values of the training data.
+        X_test (DataFrame): The feature matrix of the test data.
+        y_test (DataFrame): The target values of the test data.
+        k (int, optional): The number of neighbors to consider. Defaults to 3.
+
+    Returns:
+        tuple[np.ndarray, float, float]: A tuple containing the test predictions, train accuracy, and test accuracy.
+    """
+
+    model = KNeighborsClassifier(n_neighbors=k)
+
+    print(
+        f"{Fore.RED}X_train dim: {X_train.shape}, y_train dim: {y_train.shape}{Fore.WHITE}"
+    )
+    model.fit(X_train, y_train)
+
+    # Get results
+    train_accuracy = model.score(X_train, y_train)
+
+    test_predictions = model.predict(X_test)
+    test_accuracy = accuracy_score(y_test, test_predictions)
+
+    return test_predictions, train_accuracy, test_accuracy
