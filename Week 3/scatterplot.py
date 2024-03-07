a4a345107fe4b8e1b00eb0512e54c10e53e054a7
Hugo Burton <burto732@gmail.com>
2024-03-07 12:19:11 +1000

    Implemented scatterplot function

---
+++ Week 3/scatterplot.py
@@ -0,0 +1,24 @@
+"""
+Helper to plot dataset on a scatterplot
+"""
+
+from matplotlib.colors import ListedColormap
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def scatterplot(data):
+    fig = plt.figure()
+    colours = ListedColormap(["r", "b"])
+    scatter = plt.scatter(
+        data.loc[:, "X1"],
+        data.loc[:, "X2"],
+        c=data.loc[:, "Y"],
+        cmap=colours,
+        label=data.loc[:, "Y"],
+    )
+    plt.legend(*scatter.legend_elements())
+    plt.ylabel("X1")
+    plt.xlabel("X2")
+    plt.show()
