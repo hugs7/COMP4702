883fe3873da0d9c09773ca9d94586b10a93003fc
Hugo Burton <burto732@gmail.com>
2024-03-14 13:22:30 +1000

    Updated main to feature argv support

--- Week 4/w4prac.py
+++ Week 4/w4prac.py
@@ -2,19 +2,33 @@
 Week 4 Prac Main file
 """
 
-import numpy as np
 from q1 import q1
-
-
-def q2():
-    pass
+from q2 import q2
+import os
+import sys
 
 
 def main():
-    print("Week 4 Prac Main file")
+    current_folder = os.path.dirname(__file__)
+    data_folder = os.path.join(current_folder, "data")
 
-    # Question 1
-    q1()
+    questions = [str(i) for i in range(1, 2 + 1)]
+
+    if len(sys.argv) > 1:
+        if sys.argv[1] in questions:
+            if sys.argv[1] == "1":
+                q1()
+            elif sys.argv[1] == "2":
+                q2(data_folder)
+
+    else:
+        # Run all questions
+
+        # Question 1
+        q1()
+
+        # Question 2
+        q2(data_folder)
 
 
 if __name__ == "__main__":
