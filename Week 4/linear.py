94450bbf521661297e801c93ec7ef66bbf4adfc3
Hugo Burton <burto732@gmail.com>
2024-03-14 12:54:36 +1000

    Modified Linear model to call regressor parent and moved predict and mse function into parent

--- Week 4/linear.py
+++ Week 4/linear.py
@@ -4,41 +4,29 @@ Module for helping with performing linear regression on data
 
 import numpy as np
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
+from regressor import Regressor
 
 
-class LinearRegressionModel:
+class LinearRegressionModel(Regressor):
     """
     Class for performing linear regression on data
     """
 
-    def __init__(self, x_train: np.ndarray, y_train: np.ndarray):
+    def __init__(
+        self,
+        X_train: np.ndarray,
+        y_train: np.ndarray,
+        X_test: np.ndarray,
+        y_test: np.ndarray,
+        feature_names: list[str],
+    ):
         self.model = LinearRegression()
 
-        self.predictions = None
-        self.x_train = x_train
-        self.y_train = y_train
+        # Call the super class constructor
+        super().__init__(X_train, y_train, X_test, y_test, feature_names, self.model)
 
     def fit(self) -> None:
         """
         Fits the model to the training data
         """
-        self.model.fit(self.x_train, self.y_train)
-
-    def predict(self, x: np.ndarray) -> np.ndarray:
-        """
-        Predicts the y values for the given x values
-        """
-
-        self.predictions = self.model.predict(x)
-
-        return self.predictions
-
-    def mean_squared_error(self, y: np.ndarray) -> float:
-        """
-        Returns the mean squared error for the given x and y values
-        """
-        if self.predictions is None:
-            raise ValueError("No predictions have been made yet")
-
-        return mean_squared_error(y, self.predictions)
+        self.model.fit(self.X_train, self.y_train)
