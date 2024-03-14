07afaae3d04ed8efb2e614a0145810b340d9a0cd
Hugo Burton <burto732@gmail.com>
2024-03-14 12:35:37 +1000

    Implemented q1b. Implemented function to generate training points

--- Week 4/w4prac.py
+++ Week 4/w4prac.py
@@ -6,6 +6,23 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+def generate_training_point(f: callable, noise: float = 0.2) -> float:
+    """
+    Generates a single training point with noise from the function f
+    """
+
+    # Generate a random x value
+    x = np.random.uniform(-1, 1)
+
+    # Generate a random noise value using a gaussian distribution
+    noise = np.random.normal(0, noise)
+
+    # Generate the y value
+    y = f(x) + noise
+
+    return x, y
+
+
 def q1():
     # Part A
 
@@ -17,9 +34,21 @@ def q1():
     plt.plot(domain, f(domain))
     plt.xlabel("x")
     plt.ylabel("f(x)")
-    plt.show()
+    # plt.show()
 
     # Part B
+
+    # Generate training points
+    num_training_points = 30
+
+    train_set = {generate_training_point(f) for _ in range(num_training_points)}
+
+    print("Training set", train_set)
+
+    # Overlay training set points onto function
+    plt.scatter(*zip(*train_set), color="red")
+
+    plt.show()
 
     # Part C
 
