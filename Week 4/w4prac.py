d986dbdfd7150eae07c03a8d301bc04b6b36aa1d
Hugo Burton <burto732@gmail.com>
2024-03-18 11:59:42 +1000

    Called q3

--- Week 4/w4prac.py
+++ Week 4/w4prac.py
@@ -4,6 +4,7 @@ Week 4 Prac Main file
 
 from q1 import q1
 from q2 import q2
+from q3 import q3
 import os
 import sys
 
@@ -12,7 +13,7 @@ def main():
     current_folder = os.path.dirname(__file__)
     data_folder = os.path.join(current_folder, "data")
 
-    questions = [str(i) for i in range(1, 2 + 1)]
+    questions = [str(i) for i in range(1, 3 + 1)]
 
     if len(sys.argv) > 1:
         if sys.argv[1] in questions:
@@ -20,6 +21,8 @@ def main():
                 q1()
             elif sys.argv[1] == "2":
                 q2(data_folder)
+            elif sys.argv[1] == "3":
+                q3(data_folder)
 
     else:
         # Run all questions
@@ -29,6 +32,9 @@ def main():
 
         # Question 2
         q2(data_folder)
+
+        # Question 3
+        q3(data_folder)
 
 
 if __name__ == "__main__":
