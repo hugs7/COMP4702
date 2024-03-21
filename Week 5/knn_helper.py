1263f0a75cb455a65152293c50e6c4ac7dfcf445
Hugo Burton <burto732@gmail.com>
2024-03-21 13:26:28 +1000

    Implemented k_fold_split

--- Week 5/knn_helper.py
+++ Week 5/knn_helper.py
@@ -43,3 +43,22 @@ def knn_classify(
         )
 
     return train_accuracy, test_accuracy
+
+
+def k_fold_split(data, num_folds, fold):
+    """
+    Splits the data into training and testing data for a given fold.
+    """
+
+    # Split the data into training and testing data
+    fold_size = len(data) // num_folds
+    test_start = fold_size * fold
+    test_end = test_start + fold_size
+
+    X_test = data.iloc[test_start:test_end, :-1]
+    y_test = data.iloc[test_start:test_end, -1]
+
+    X_train = data.drop(data.index[test_start:test_end]).iloc[:, :-1]
+    y_train = data.drop(data.index[test_start:test_end]).iloc[:, -1]
+
+    return X_train, y_train, X_test, y_test
