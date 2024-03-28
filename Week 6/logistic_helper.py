f711d9b41a5c8da69daa9aa9d39acc8bc83c0037
hugs7 <burto732@gmail.com>
2024-03-28 13:50:40 +1000

    Implemented helper for logistic model to fit data and get results

---
+++ Week 6/logistic_helper.py
@@ -0,0 +1,24 @@
+
+from pandas import DataFrame
+import logistic
+
+
+def logistic_fit(X_train: DataFrame, y_train: DataFrame, X_test: DataFrame,
+                 y_test: DataFrame, feature_names: list[str], classes: list[str], threshold: float = 0.5) \
+        -> DataFrame:
+    """
+    Classification task using logistic regression
+    """
+
+    # Apply the logistic classifier
+    logistic_classifier = logistic.LogisticRegressionModel(
+        X_train, y_train, X_test, y_test, feature_names
+    )
+
+    logistic_classifier.fit()
+
+    # Get results
+    test_predictions, train_accuracy, test_accuracy = logistic_classifier.predict(classes,
+                                                                                  threshold=threshold)
+
+    return test_predictions, train_accuracy, test_accuracy
