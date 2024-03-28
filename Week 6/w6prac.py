8985af5484f2f83ddb26a71124df61e4ca3c314f
hugs7 <burto732@gmail.com>
2024-03-28 13:05:40 +1000

    Added tags to the data so columns are labelled Trained and classified data Returned accuracy for now

--- Week 6/w6prac.py
+++ Week 6/w6prac.py
@@ -3,9 +3,45 @@ Driver file for week 6 practical class
 28/03/2024
 """
 
-from load_data import load_data
+from load_data import load_data, split_feature_response, tag_data
 import os
 from colorama import Fore, Style
+from pandas import DataFrame
+from knn_helper import knn_classify
+
+
+def q1(train_data: DataFrame, test_data: DataFrame):
+    """
+    Train a k-NN model (choose some reasonable value for k) on the training set and calculate a 
+    confusion matrix for the hold-out validation set.
+    """
+
+    # train_data will be used as training set.
+    # test_data will be used as final test set (called hold-out in the question).
+
+    # Extract the feature names
+    feature_names = train_data.columns[:-1]
+    X_train, y_train = split_feature_response(train_data)
+    X_test, y_test = split_feature_response(test_data)
+
+    # Apply the knn classifier to the training data
+    test_predictions, train_accuracy, test_accuracy = knn_classify(
+        X_train, y_train, X_test, y_test, feature_names, k=5
+    )
+
+    print(f"{Fore.LIGHTGREEN_EX}Train Accuracy: {Style.RESET_ALL}{train_accuracy}")
+    print(f"{Fore.LIGHTGREEN_EX}Test Accuracy: {Style.RESET_ALL}{test_accuracy}")
+
+    return 0
+
+
+def q2():
+    return 0
+
+
+def q3():
+    return 0
+
 
 def main():
     current_folder = os.path.dirname(__file__)
@@ -21,11 +57,20 @@ def main():
     train_data = load_data(train_datafile_path)
     test_data = load_data(test_datafile_path)
 
+    # Tag the data
+    columns = [f"X{i}" for i in range(1, train_data.shape[1])] + ["Y"]
+    print(f"{Fore.LIGHTMAGENTA_EX}Data Columns: {Style.RESET_ALL}{columns}\n")
+    train_data = tag_data(train_data, columns)
+    test_data = tag_data(test_data, columns)
+
     print(f"{Fore.GREEN}Train data:{Style.RESET_ALL}")
     print(train_data.head())
 
     print(f"{Fore.GREEN}Test data:{Style.RESET_ALL}")
     print(test_data.head())
+
+    # Question 1
+    q1(train_data, test_data)
 
     exit(0)
 
