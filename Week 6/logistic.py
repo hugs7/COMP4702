bdc04924f60601b49c465ceadbb6341c279dabb5
hugs7 <burto732@gmail.com>
2024-03-28 13:50:30 +1000

    Implemented logistic model

---
+++ Week 6/logistic.py
@@ -0,0 +1,53 @@
+"""
+Defines logistic model
+"""
+
+import numpy as np
+
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+
+from regressor import Regressor
+
+
+class LogisticRegressionModel(Regressor):
+    """
+    Class for performing logistic regression
+    """
+
+    def __init__(
+        self,
+        X_train: np.ndarray,
+        y_train: np.ndarray,
+        X_test: np.ndarray,
+        y_test: np.ndarray,
+        feature_names: list[str],
+    ):
+        self.model = LogisticRegression()
+
+        # Call the super class constructor
+        super().__init__(X_train, y_train, X_test, y_test, feature_names, self.model)
+
+    def fit(self) -> None:
+        """
+        Fits the model to the training data
+        """
+        self.model.fit(self.X_train, self.y_train)
+
+    def predict(self, classes: list[str], threshold: float = 0.5) -> np.ndarray:
+        """
+        Predicts the test data
+        """
+        test_probabilities = self.model.predict_proba(self.X_test)
+
+        # Apply threshold to determine predicted class
+        test_predictions_num = (
+            test_probabilities[:, 1] > threshold).astype(int)
+        test_predictions = np.array(classes)[test_predictions_num]
+
+        # Get results
+        train_accuracy = self.model.score(self.X_train, self.y_train)
+
+        test_accuracy = accuracy_score(self.y_test, test_predictions)
+
+        return test_predictions, train_accuracy, test_accuracy
