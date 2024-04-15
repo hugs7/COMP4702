73a7b05669a37348d56bd9e242a5e1bb2df2f066
hugs7 <burto732@gmail.com>
2024-04-15 23:29:03 +1000

    Added colours to welcome message

--- Week 7/welcome.py
+++ Week 7/welcome.py
@@ -3,6 +3,7 @@ import torch
 import sys
 import numpy as np
 import seaborn as sb
+from colorama import Fore, Style
 
 
 def welcome():
@@ -12,15 +13,18 @@ def welcome():
 
     # Check if CUDA is available
     if torch.cuda.is_available():
-        print("CUDA is available")
-        print("Device count: ", torch.cuda.device_count())
-        print("Current device: ", torch.cuda.current_device())
-        print("Device name: ", torch.cuda.get_device_name())
+        print(f"{Fore.LIGHTGREEN_EX}CUDA is available{Style.RESET_ALL}")
+        print(
+            f"{Fore.LIGHTMAGENTA_EX}Device count: {Style.RESET_ALL}{torch.cuda.device_count()}")
+        print(
+            f"{Fore.LIGHTMAGENTA_EX}Current device: {Style.RESET_ALL}{torch.cuda.current_device()}")
+        print(
+            f"{Fore.LIGHTMAGENTA_EX}Device name: {Style.RESET_ALL}{torch.cuda.get_device_name()}")
     else:
-        print("CUDA is not available")
+        print(f"{Fore.LIGHTRED_EX}CUDA is not available{Style.RESET_ALL}")
 
-    print("Python version: ", sys.version)
-    print("Numpy version: ", np.__version__)
-    print("Seaborn version: ", sb.__version__)
+    print(f"{Fore.LIGHTCYAN_EX}Python version: {Style.RESET_ALL}{sys.version}")
+    print(f"{Fore.LIGHTCYAN_EX}Numpy version: {Style.RESET_ALL}{np.__version__}")
+    print(f"{Fore.LIGHTCYAN_EX}Seaborn version: {Style.RESET_ALL}{sb.__version__}")
 
     print("-" * 40)
