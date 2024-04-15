de8a4a62159e6d8bf443891cf88f46e220bc3424
hugs7 <burto732@gmail.com>
2024-04-15 11:20:23 +1000

    Moved model definition into separate file

--- Week 7/main.py
+++ Week 7/main.py
@@ -10,7 +10,9 @@ import seaborn as sb
 import numpy as np
 from typing import List
 import torchvision
+
 from plot import lineplot, plot_gelu, plot_sigmoid
+import model
 
 
 def welcome():
@@ -32,48 +34,6 @@ def welcome():
     print("Seaborn version: ", sb.__version__)
 
     print("-" * 40)
-
-
-def create_sequential_model(dim_input: int, dim_output: int, hidden_layer_dims: List[int]) -> torch.nn.Sequential:
-    """
-    Create a sequential model
-
-    Args:
-        dim_input: Dimension of the input
-        dim_output: Dimension of the output
-        hidden_layers: List of hidden layers
-
-    Returns:
-        Sequential model
-    """
-
-    print("Input dimension: ", dim_input)
-
-    hiddens = [dim_input, *hidden_layer_dims]
-
-    torch_layers = []
-
-    # Create a linear layer and feed it through the activation function
-    for i in range(len(hiddens) - 1):
-        # Create linear layer from i to i+1
-        linear_layer = torch.nn.Linear(hiddens[i], hiddens[i+1])
-
-        # Create activation function
-        activation = torch.nn.GELU()
-
-        # Add to the list
-        torch_layers.append(linear_layer)
-        torch_layers.append(activation)
-
-    # Add the final output layer
-    final_hidden_layer = hiddens[-1]
-    output_layer = torch.nn.Linear(final_hidden_layer, dim_output)
-    torch_layers.append(output_layer)
-
-    # Turn the list into a sequential model
-    sequential_model = torch.nn.Sequential(*torch_layers)
-
-    return sequential_model
 
 
 def nn_train(epoch: int, train_data: np.ndarray, train_labels: np.ndarray, batch_size: int,
@@ -184,7 +144,7 @@ def main():
     dim_in = 32*32*3
     dim_out = 10
     hidden_layer_dims = [100, 100]
-    sequential_model = create_sequential_model(
+    sequential_model = model.create_sequential_model(
         dim_in, dim_out, hidden_layer_dims)
 
     print(sequential_model)
