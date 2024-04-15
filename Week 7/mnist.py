86a19c531053f2cba7e94661d79db206cdb64988
hugs7 <burto732@gmail.com>
2024-04-15 18:28:04 +1000

    Added mnist model and renamed main to cifar10

--- Week 7/mnist.py
+++ Week 7/mnist.py
@@ -1,6 +1,6 @@
 """
 Week 7 Prac
-CIFAR10 dataset classification
+MNIST dataset classification
 11/04/2024
 """
 
@@ -11,11 +11,11 @@ from welcome import welcome
 def main():
     welcome()
 
-    dim_in = 32*32*3
-    normalising_factor = 255.0
+    dim_in = 28*28*1
+    normalising_factor = 1.0
     dim_out = 10
     hidden_layer_dims = [100, 100]
-    classification_model("CIFAR10", dim_in, dim_out,
+    classification_model("MNIST", dim_in, dim_out,
                          hidden_layer_dims, normalising_factor)
 
 
