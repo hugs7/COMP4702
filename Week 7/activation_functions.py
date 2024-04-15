88fdcce71163e9b85b410e1fd14964336be4f83b
hugs7 <burto732@gmail.com>
2024-04-15 18:27:49 +1000

    Moved activation functions to own file

---
+++ Week 7/activation_functions.py
@@ -0,0 +1,13 @@
+
+from plot import plot_gelu, plot_sigmoid
+
+
+def main():
+    # Check length of command line arguments
+
+    input = plot_gelu()
+    plot_sigmoid(input)
+
+
+if __name__ == "__main__":
+    main()
