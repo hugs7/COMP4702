e87b211a06b02b0627ef92160bf40cf7e0ae05bd
hugs7 <burto732@gmail.com>
2024-04-16 18:17:19 +1000

    Renamed loss to accuracy

--- Project/results.py
+++ Project/results.py
@@ -7,13 +7,13 @@ def show_training_results(metrics):
     metrics = np.asarray(metrics)
 
     # Training Loss
-    training_loss_x = metrics[:, 0]
-    training_loss_y = metrics[:, 2]
-    train_metrics = (training_loss_x, training_loss_y)
+    training_accuracy_x = metrics[:, 0]
+    training_accuracy_y = metrics[:, 2]
+    train_metrics = (training_accuracy_x, training_accuracy_y)
 
     # Validation Loss
     # validation_loss_x = metrics[:, 0]
     # validation_loss_y = metrics[:, 3]
     # validation_metrics = (validation_loss_x, validation_loss_y)
 
-    lineplot("Epoch", "Loss", train_metrics)
+    lineplot("Epoch", "Accuracy", train_metrics)
