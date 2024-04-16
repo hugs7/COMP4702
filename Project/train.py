d031fbd3bdcf89a92adf54e02a51ef9b3d530792
hugs7 <burto732@gmail.com>
2024-04-16 21:08:40 +1000

    Added reshape of predictions so we can predict in 2D with multiple classes.
    Recovered variables to compute loss and did a similar thing for the accuracy metrics.

--- Project/train.py
+++ Project/train.py
@@ -13,7 +13,7 @@ def nn_train(
     optimiser: torch.optim.SGD,
     optimisation_steps: int,
     metrics: List[float],
-    dim_output: int,
+    classes_in_output_vars: int,
     log: bool = False,
 ) -> List:
     """
@@ -28,7 +28,7 @@ def nn_train(
         criterion: Loss function
         optimiser: Optimiser
         optimisation_steps: Number of steps to train
-        dim_output: Dimension of the output
+        classes_in_output_vars: List[int]: number of classes in each output variable - used for reshaping the output
         metrics: List of metrics
 
     Returns:
@@ -46,6 +46,21 @@ def nn_train(
 
     # Make predictions
     y_pred = sequential_model(x)
+
+    # As we (potentially) have multidimensional output, we need to reshape the predictions
+    # or unflatten them based on classes_in_output_vars
+
+    reshaped_preds = []
+    sample = 5
+    cum = 0
+    for i, recovered_var_dim in enumerate(classes_in_output_vars):
+        previous = classes_in_output_vars[i - 1] if i > 0 else 0
+        cum += previous
+
+        reshaped_pred = y_pred[:, previous:cum+recovered_var_dim]
+
+        reshaped_preds.append(reshaped_pred)
+
     if log:
         print("Predictions: ", y_pred)
         print("Shape of predictions: ", y_pred.shape)
@@ -54,12 +69,16 @@ def nn_train(
     # True labels
     if log:
         print("True labels:")
+
     train_labels_batch = train_labels[indices]
+
     if log:
         print(train_labels_batch)
         print("Shape of labels: ", train_labels_batch.shape)
         print()
+
     y_true = train_labels_batch
+
     # Convert to long tensor
     # y_true = y_true.long()
     if log:
@@ -68,7 +87,27 @@ def nn_train(
         print()
 
     # Compute the loss
-    loss = criterion(y_pred, y_true)
+    if log:
+        # print("y_pred: ", y_pred)
+        print("y_resh: ", reshaped_preds)
+        print("y_true: ", y_true)
+
+    # Compute the loss for each recovered variable
+    losses = []
+    for dim, recovered_var_dim in enumerate(reshaped_preds):
+        y_true_dim = y_true[:, dim].long()  # convert to int64
+        if log:
+            print("dim: ", dim, "recov: ",
+                  recovered_var_dim, "true: ", y_true_dim)
+            print(recovered_var_dim.shape, y_true_dim.shape)
+            print(recovered_var_dim.dtype, y_true_dim.dtype)
+        loss = criterion(recovered_var_dim, y_true_dim)
+        losses.append(loss)
+        if log:
+            print(loss)
+
+    # Loss is the sum of all losses
+    loss = sum(losses)
 
     # Zero the gradients
     optimiser.zero_grad()
@@ -83,14 +122,31 @@ def nn_train(
         if log:
             print(f"Epoch: {epoch} / {optimisation_steps}")
             print("Loss: ", loss.item())
-        print("y pred: ", y_pred)
-        argmax = y_pred.argmax(dim=dim_output)
-        print("Argmax: ", argmax)
-        print("Shape of argmax: ", argmax.shape)
-        print("Y true: ", y_true)
 
-        train_accuracy = torch.mean((argmax == y_true).float())
+        # Find argument which maximises the prediction value
+        # Again we need to reshape the predictions and take argmax of each recovered variable
+        accuracies = []
+        for dim, recovered_var_dim in enumerate(reshaped_preds):
+            if log:
+                print(f"Dim {dim}, Reshaped prefs: ",
+                      recovered_var_dim, recovered_var_dim.shape, recovered_var_dim.dtype)
+            y_true_dim = y_true[:, dim].long()
+            argmax = recovered_var_dim.argmax(dim=1)
+            print("Argmax: ", argmax)
 
-        metrics.append([epoch, loss.item(), train_accuracy.cpu().numpy()])
+            # Comparison
+            comparison = argmax == y_true_dim
+            if log:
+                print("Comparison: ", comparison)
+
+            train_accuracy = torch.mean((comparison).float())
+            train_accuracy_value = train_accuracy.cpu().numpy()
+            accuracies.append(train_accuracy_value)
+
+        # Take average of all accuracies
+        train_accuracy = sum(accuracies) / len(accuracies)
+        print("Train accuracy: ", train_accuracy)
+
+        metrics.append([epoch, loss.item(), train_accuracy])
 
     return metrics
