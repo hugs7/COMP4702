7d9508be9657a864d3e9d273a7a4266b22401d4f
hugs7 <burto732@gmail.com>
2024-04-16 16:50:28 +1000

    Added encoding helper

---
+++ Project/encode_data.py
@@ -0,0 +1,22 @@
+"""
+Helper function to encode non-numeric data as numeric
+"""
+
+import pandas as pd
+
+
+def encode_non_numeric_data(data: pd.DataFrame) -> pd.DataFrame:
+    # Create a copy of the data
+    data = data.copy()
+
+    for column in data.columns:
+        sample_data_point = data[column].iloc[0]
+        print(
+            f"Column: {column}, Sample Data Point: {sample_data_point}, Data Type: {data[column].dtype}"
+        )
+        if data[column].dtype == str or data[column].dtype == object:
+            # Replace with mode
+            data[column] = data[column].astype("category")
+            data[column] = data[column].cat.codes
+
+    return data
