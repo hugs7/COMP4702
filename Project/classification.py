387ec5091e60358f3a16eafb04a9a430dbd3f5fd
hugs7 <burto732@gmail.com>
2024-04-16 16:51:05 +1000

    Formatted code

--- Project/classification.py
+++ Project/classification.py
@@ -1,4 +1,3 @@
-
 from typing import List, Tuple
 import model
 from colorama import Fore, Style
@@ -11,14 +10,19 @@ from results import show_training_results
 from tqdm import tqdm
 
 
-def preprocess_data(train_data: np.ndarray, validation_data: np.ndarray, dim_input: int,
-                    normalising_factor: float) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
+def preprocess_data(
+    train_data: np.ndarray,
+    validation_data: np.ndarray,
+    dim_input: int,
+    normalising_factor: float,
+) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
 
     return_data = []
 
     for data in [train_data, validation_data]:
-        new_data = torch.as_tensor(data.data.reshape((-1, dim_input)) /
-                                   normalising_factor, dtype=torch.float32)
+        new_data = torch.as_tensor(
+            data.data.reshape((-1, dim_input)) / normalising_factor, dtype=torch.float32
+        )
         labels = torch.as_tensor(data.targets)
 
         return_data.append(new_data)
@@ -27,9 +31,16 @@ def preprocess_data(train_data: np.ndarray, validation_data: np.ndarray, dim_inp
     return return_data
 
 
-def classification_model(dataset_name: str, dim_input: int, dim_output: int, hidden_layer_dims: List[int],
-                         normalising_factor: float, optimisation_steps: int = int(1e4), batch_size: int = 256,
-                         learning_rate: float = 1e-3) -> None:
+def classification_model(
+    dataset_name: str,
+    dim_input: int,
+    dim_output: int,
+    hidden_layer_dims: List[int],
+    normalising_factor: float,
+    optimisation_steps: int = int(1e4),
+    batch_size: int = 256,
+    learning_rate: float = 1e-3,
+) -> None:
     print(f"{Fore.GREEN}Creating model for {dataset_name} dataset{Style.RESET_ALL}")
 
     # Move model to GPU if available
@@ -37,7 +48,8 @@ def classification_model(dataset_name: str, dim_input: int, dim_output: int, hid
 
     # Instantiate the model and move it to the specified device
     sequential_model = model.create_sequential_model(
-        dim_input, dim_output, hidden_layer_dims).to(device)
+        dim_input, dim_output, hidden_layer_dims
+    ).to(device)
 
     print(sequential_model)
 
@@ -82,16 +94,17 @@ def classification_model(dataset_name: str, dim_input: int, dim_output: int, hid
     }
 
     if dataset_name not in dataset_map:
-        raise ValueError(
-            f"{Fore.RED}Dataset {dataset_name} not found{Style.RESET_ALL}")
+        raise ValueError(f"{Fore.RED}Dataset {dataset_name} not found{Style.RESET_ALL}")
 
     # Get the dataset
     mappped_dataset = dataset_map[dataset_name]
 
     train_data = mappped_dataset(
-        model_data_folder, download=download, train=True, transform=True)
+        model_data_folder, download=download, train=True, transform=True
+    )
     validation_data = mappped_dataset(
-        model_data_folder, download=download, train=False, transform=True)
+        model_data_folder, download=download, train=False, transform=True
+    )
 
     print("Training data stats")
     print("Shape:", train_data.data.shape)
@@ -101,7 +114,8 @@ def classification_model(dataset_name: str, dim_input: int, dim_output: int, hid
     # We flatten the dataset because we need to pass in a 1D array
     # not a 3D array (which is (X, Y, Channels))
     train_data, train_labels, validation_data, validation_labels = preprocess_data(
-        train_data, validation_data, dim_input, normalising_factor)
+        train_data, validation_data, dim_input, normalising_factor
+    )
 
     # Move data to device
     train_data = train_data.to(device)
@@ -117,14 +131,22 @@ def classification_model(dataset_name: str, dim_input: int, dim_output: int, hid
 
     criterion = torch.nn.CrossEntropyLoss(reduction="mean")
 
-    optimiser = torch.optim.SGD(
-        sequential_model.parameters(), lr=learning_rate)
+    optimiser = torch.optim.SGD(sequential_model.parameters(), lr=learning_rate)
 
     # --- Training Loop ---
 
     metrics = []
     for i in tqdm(range(int(optimisation_steps))):
-        metrics = train.nn_train(i, train_data, train_labels, batch_size,
-                                 sequential_model, criterion, optimiser, optimisation_steps, metrics)
+        metrics = train.nn_train(
+            i,
+            train_data,
+            train_labels,
+            batch_size,
+            sequential_model,
+            criterion,
+            optimiser,
+            optimisation_steps,
+            metrics,
+        )
 
     show_training_results(metrics)
