3911540918aa269e265dcbe87c71d80ec7705a67
Hugo Burton <burto732@gmail.com>
2024-04-18 12:30:56 +1000

    Initial commit week 8 practical

---
+++ Week 8/cnn.py
@@ -0,0 +1,16 @@
+"""
+Week 8 Practical
+Driver File
+18/04/2024
+"""
+
+from welcome import welcome
+
+
+def main():
+    welcome()
+    exit(0)
+
+
+if __name__ == "__main__":
+    main()
---
+++ Week 8/welcome.py
@@ -0,0 +1,30 @@
+
+import torch
+import sys
+import numpy as np
+import seaborn as sb
+from colorama import Fore, Style
+
+
+def welcome():
+    print("Week 7: PyTorch")
+
+    print("Torch version: ", torch.__version__)
+
+    # Check if CUDA is available
+    if torch.cuda.is_available():
+        print(f"{Fore.LIGHTGREEN_EX}CUDA is available{Style.RESET_ALL}")
+        print(
+            f"{Fore.LIGHTMAGENTA_EX}Device count: {Style.RESET_ALL}{torch.cuda.device_count()}")
+        print(
+            f"{Fore.LIGHTMAGENTA_EX}Current device: {Style.RESET_ALL}{torch.cuda.current_device()}")
+        print(
+            f"{Fore.LIGHTMAGENTA_EX}Device name: {Style.RESET_ALL}{torch.cuda.get_device_name()}")
+    else:
+        print(f"{Fore.LIGHTRED_EX}CUDA is not available{Style.RESET_ALL}")
+
+    print(f"{Fore.LIGHTCYAN_EX}Python version: {Style.RESET_ALL}{sys.version}")
+    print(f"{Fore.LIGHTCYAN_EX}Numpy version: {Style.RESET_ALL}{np.__version__}")
+    print(f"{Fore.LIGHTCYAN_EX}Seaborn version: {Style.RESET_ALL}{sb.__version__}")
+
+    print("-" * 40)
