a178f9b60723594b2d2c05796dd007a64ad570e7
Hugo Burton <burto732@gmail.com>
2024-04-23 15:25:42 +1000

    Added knn model to project

---
+++ Project/knn/classifier.py
@@ -0,0 +1,96 @@
+from base_model import Model
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+import pandas as pd
+from pandas import DataFrame
+import numpy as np
+from sklearn.base import BaseEstimator
+
+
+class Classifier(Model):
+    def __init__(
+        self,
+        X_train: DataFrame,
+        y_train: DataFrame,
+        X_test: DataFrame,
+        y_test: DataFrame,
+        feature_names: list[str],
+        model: BaseEstimator,
+    ) -> None:
+        super().__init__(X_train, y_train, X_test, y_test, feature_names)
+
+        self.model = model
+
+    def plot_decision_regions(
+        self,
+        test_preds: np.ndarray,
+        resolution=0.02,
+        plot_title="Decision Regions",
+    ) -> None:
+        """
+        Plots the decision regions for a classifier.
+
+        Parameters:
+        - X_test (DataFrame): The input data used for testing the classifier.
+        - test_preds (ndarray): The predicted labels for the test data.
+        - resolution (float): The step size of the mesh grid used for plotting the decision regions. Default is 0.02.
+
+        Returns:
+        - None
+
+        This function plots the decision regions for a classifier by creating a mesh grid based on the input data and
+        classifying each point in the grid. The decision regions are then visualized using a contour plot.
+
+        Note:
+        - The input data should have exactly two features for proper visualization.
+        - The classifier should have a `predict` method that takes a feature matrix as input and returns the predicted labels.
+        """
+
+        X1_test = self.X_test.iloc[:, 0]
+        X2_test = self.X_test.iloc[:, 1]
+
+        print(X1_test.shape)
+        print(X2_test.shape)
+
+        # Print the range of your input features
+        print("X1 Range:", X1_test.min(), "-", X1_test.max())
+        print("X2 Range:", X2_test.min(), "-", X2_test.max())
+
+        # Generate a meshgrid of points to cover the feature space
+        x_min, x_max = X1_test.min() - 0.5, X1_test.max() + 0.5
+        y_min, y_max = X2_test.min() - 0.5, X2_test.max() + 0.5
+
+        print(x_min, x_max, y_min, y_max)
+
+        xx, yy = np.meshgrid(
+            np.arange(x_min, x_max, resolution), np.arange(y_min, y_max, resolution)
+        )
+
+        print(xx.shape, "|", yy.shape)
+
+        feature_names = ["X1", "X2"]
+        Z = pd.DataFrame(np.c_[xx.ravel(), yy.ravel()], columns=feature_names)
+
+        Z_preds = self.model.predict(Z)
+
+        print("Before reshape Z_pred:", Z_preds.shape)
+
+        Z_preds = Z_preds.reshape(xx.shape)
+
+        print(Z_preds.shape)
+
+        # Plot the decision boundary
+        cmap_light = ListedColormap(["#FFAAAA", "#AAFFAA", "#AAAAFF"])
+        plt.pcolormesh(xx, yy, Z_preds, cmap=cmap_light, shading="auto")
+
+        # Overlay the test points
+        cmap_bold = ListedColormap(["#FF0000", "#00FF00"])
+        plt.scatter(X1_test, X2_test, c=test_preds, cmap=cmap_bold)
+
+        # Setup plot
+        plt.xlim(xx.min(), xx.max())
+        plt.ylim(yy.min(), yy.max())
+        plt.xlabel("X1")
+        plt.ylabel("X2")
+        plt.title(plot_title)
+        plt.show()
---
+++ Project/knn/knn.py
@@ -0,0 +1,58 @@
+"""
+Implements a KNN Classification model
+"""
+
+from classifier import Classifier
+from colorama import Fore, Style
+import numpy as np
+import pandas as pd
+from pandas import DataFrame
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+
+
+class KNNClassify(Classifier):
+    def __init__(
+        self,
+        X_train: DataFrame,
+        y_train: DataFrame,
+        X_test: DataFrame,
+        y_test: DataFrame,
+        feature_names: list[str],
+        k: int = 3,
+    ):
+        self.k = k
+        # Define Model
+        self.model = KNeighborsClassifier(n_neighbors=self.k)
+
+        super().__init__(X_train, y_train, X_test, y_test, feature_names, self.model)
+
+    def get_k(self) -> int:
+        return self.k
+
+    def classify(
+        self,
+    ) -> tuple[np.ndarray, float, float]:
+        """
+        Performs k-nearest neighbors classification on the given training and test data.
+
+        Returns:
+            tuple[KNeighborsClassifier, np.ndarray, float, float]: A tuple containing the knn classifier, test predictions, train accuracy, and test accuracy.
+        """
+
+        print(
+            f"{Fore.RED}X_train dim: {self.X_train.shape}, y_train dim: {self.y_train.shape}{Style.RESET_ALL}"
+        )
+
+        X_train_df = pd.DataFrame(self.X_train, columns=self.feature_names)
+        X_test_df = pd.DataFrame(self.X_test, columns=self.feature_names)
+
+        self.model.fit(X_train_df, self.y_train)
+
+        # Get results
+        train_accuracy = self.model.score(self.X_train, self.y_train)
+
+        test_predictions = self.model.predict(self.X_test)
+        test_accuracy = accuracy_score(self.y_test, test_predictions)
+
+        return test_predictions, train_accuracy, test_accuracy
