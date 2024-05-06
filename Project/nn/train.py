39fc9ffa0ad0d406229eecbe2c5a1a671cc47e4c
Hugo Burton <burto732@gmail.com>
2024-05-06 12:50:06 +1000

    Improved logging in train function for nn

--- Project/nn/train.py
+++ Project/nn/train.py
@@ -2,6 +2,7 @@ import numpy as np
 from typing import List
 import torch
 
+from logger import *
 
 CUDA = "cuda"
 CPU = "cpu"
@@ -18,35 +19,32 @@ def nn_train(
     optimisation_steps: int,
     metrics: List[float],
     classes_in_output_vars: int,
-    log: bool = False,
 ) -> List:
     """
     One epoch of training
 
     Args:
-        epoch: Current epoch
-        train_data: Training data
-        train_labels: Training labels
-        batch_size: Batch size
-        sequential_model: Model
-        criterion: Loss function
-        optimiser: Optimiser
-        optimisation_steps: Number of steps to train
-        classes_in_output_vars: List[int]: number of classes in each output variable - used for reshaping the output
-        metrics: List of metrics
+    - epoch (int): Current epoch
+    - train_data (torch.Tensor): Training data
+    - train_labels (torch.Tensor): Training labels
+    - batch_size (int): Batch size
+    - sequential_model (torch.nn.Sequential): Model object
+    - criterion (torch.nn.CrossEntropyLoss): Loss function
+    - optimiser (torch.optim.SGD): Optimiser
+    - optimisation_steps (int): Number of steps to train
+    - classes_in_output_vars (List[float]): number of classes in each output variable - used for reshaping the output
+    - metrics (int): List of metrics
 
     Returns:
-        New list of metrics
+    - List: List of metrics with the new metrics appended
     """
     # Select a random batch of data
     indices = np.random.randint(0, train_data.shape[0], size=batch_size)
 
     # Obtain the data and labels for the batch
     x = train_data[indices, :]
-    if log:
-        print(x)
-        print("Shape of x: ", x.shape)
-        print()
+
+    log_trace("Shape of x: ", x.shape)
 
     # Make predictions
     y_pred = sequential_model(x)
@@ -65,49 +63,44 @@ def nn_train(
 
         reshaped_preds.append(reshaped_pred)
 
-    if log:
-        print("Predictions: ", y_pred)
-        print("Shape of predictions: ", y_pred.shape)
-        print()
+    log_trace("Predictions: ", y_pred)
+    log_debug("Shape of predictions: ", y_pred.shape)
 
     # True labels
-    if log:
-        print("True labels:")
+
+    log_trace("True labels:")
 
     train_labels_batch = train_labels[indices]
 
-    if log:
-        print(train_labels_batch)
-        print("Shape of labels: ", train_labels_batch.shape)
-        print()
+    log_trace(train_labels_batch)
+    log_debug("Shape of labels: ", train_labels_batch.shape)
 
     y_true = train_labels_batch
 
     # Convert to long tensor
     # y_true = y_true.long()
-    if log:
-        print("True labels tensor: ", y_true)
-        print("Shape of true labels tensor: ", y_true.shape)
-        print()
+
+    log_trace("True labels tensor: ", y_true)
+    log_trace("Shape of true labels tensor: ", y_true.shape)
 
     # Compute the loss
-    if log:
-        # print("y_pred: ", y_pred)
-        print("y_resh: ", reshaped_preds)
-        print("y_true: ", y_true)
+
+    # log_trace("y_pred: ", y_pred)
+    log_trace("y_resh: ", reshaped_preds)
+    log_trace("y_true: ", y_true)
 
     # Compute the loss for each recovered variable
     losses = []
     for dim, recovered_var_dim in enumerate(reshaped_preds):
         y_true_dim = y_true[:, dim].long()  # convert to int64
-        if log:
-            print("dim: ", dim, "recov: ", recovered_var_dim, "true: ", y_true_dim)
-            print(recovered_var_dim.shape, y_true_dim.shape)
-            print(recovered_var_dim.dtype, y_true_dim.dtype)
+
+        log_trace("dim: ", dim, "recov: ", recovered_var_dim, "true: ", y_true_dim)
+        log_trace(recovered_var_dim.shape, y_true_dim.shape)
+        log_trace(recovered_var_dim.dtype, y_true_dim.dtype)
         loss = criterion(recovered_var_dim, y_true_dim)
         losses.append(loss)
-        if log:
-            print(loss)
+
+    log_debug(loss)
 
     # Loss is the sum of all losses
     loss = sum(losses)
@@ -122,25 +115,25 @@ def nn_train(
     optimiser.step()
 
     if epoch % 100 == 0 or epoch == optimisation_steps - 1:
-        if log:
-            print(f"Epoch: {epoch} / {optimisation_steps}")
-            print("Loss: ", loss.item())
+
+        log_debug(f"Epoch: {epoch} / {optimisation_steps}")
+        log_debug("Loss: ", loss.item())
 
         # Find argument which maximises the prediction value
         # Again we need to reshape the predictions and take argmax of each recovered variable
         accuracies = []
         for dim, recovered_var_dim in enumerate(reshaped_preds):
-            if log:
-                print(f"Dim {dim}, Reshaped prefs: ", recovered_var_dim, recovered_var_dim.shape, recovered_var_dim.dtype)
+
+            log_debug(f"Dim {dim}, Reshaped prefs: ", recovered_var_dim, recovered_var_dim.shape, recovered_var_dim.dtype)
             y_true_dim = y_true[:, dim].long()
             argmax = recovered_var_dim.argmax(dim=1)
-            if log:
-                print("Argmax: ", argmax)
+
+            log_debug("Argmax: ", argmax)
 
             # Comparison
             comparison = argmax == y_true_dim
-            if log:
-                print("Comparison: ", comparison)
+
+            log_debug("Comparison: ", comparison)
 
             train_accuracy = torch.mean((comparison).float())
             train_accuracy_value = train_accuracy.cpu().numpy()
@@ -148,7 +141,7 @@ def nn_train(
 
         # Take average of all accuracies
         train_accuracy = sum(accuracies) / len(accuracies)
-        print(f"Epoch: {epoch} / {optimisation_steps}, Train accuracy: {train_accuracy}, Loss: {loss.item()}")
+        log_info(f"Epoch: {epoch} / {optimisation_steps}, Train accuracy: {train_accuracy}, Loss: {loss.item()}")
 
         metrics.append([epoch, loss.item(), train_accuracy])
 
