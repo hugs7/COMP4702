f894d8954ceb0691743deeb42556e7c07a5fb488
Hugo Burton <burto732@gmail.com>
2024-05-09 13:32:44 +1000

    Improved variable importance code to give a ranking for each feature

--- Project/knn/variable_importance.py
+++ Project/knn/variable_importance.py
@@ -2,6 +2,7 @@
 Helper function to compute the importance of features in the dataset
 """
 
+from typing import Union
 from sklearn.neighbors import NearestNeighbors
 import numpy as np
 
@@ -21,32 +22,121 @@ def compute_feature_importance(X: np.ndarray, sample_idx: int, k: int, num_rando
     Returns:
         feature_importance (numpy array): Feature importance scores.
     """
+
+    def point_get_nearest_neighbors(neigh: NearestNeighbors, sample_point: np.ndarray) -> np.ndarray:
+        """
+        Get the indices of the k-nearest neighbors of a sample in the dataset.
+
+        Parameters:
+            NearestNeighbors (NearestNeighbors): A fitted nearestNeighbors model object.
+            sample_point (numpy array): The sample for which to find the nearest neighbors.
+
+        Returns:
+            neighb_indices (numpy array): Indices of the k-nearest neighbors of the sample.
+        """
+
+        _, neighb_indices = neigh.kneighbors(sample_point.reshape(1, -1))
+        neighb_indices = neighb_indices[0]
+
+        # Remove the sample itself from the neighbors
+        neighb_indices = neighb_indices[neighb_indices != sample_idx]
+
+        return neighb_indices
+
+    def point_to_point_dist(x1: np.ndarray, x2: np.ndarray, feature_index: Union[int, None]) -> float:
+        """
+        Compute the Euclidean distance between two points.
+
+        Parameters:
+            x1 (numpy array): First point.
+            x2 (numpy array): Second point.
+            feature_index (Union[int, None]): Index of the feature to compute distances for. If none, compute the distance between all features.
+
+        Returns:
+            distance (float): Euclidean distance between the two points.
+        """
+
+        if feature_index:
+            x1_feature = x1[feature_index]
+            x2_feature = x2[feature_index]
+
+            return np.abs(x1_feature - x2_feature)
+        else:
+            return np.linalg.norm(x1 - x2)
+
+    def point_to_points_distances(X: np.ndarray, point: np.ndarray, points: np.ndarray, feature_index: int) -> np.ndarray:
+        """
+        Compute the distances between a point and its neighbors.
+
+        Parameters:
+            X (numpy array): Input data matrix (features).
+            point (numpy array): The point for which to compute distances.
+            points (numpy array): The points to which to compute distances.
+            feature_index (int): The index of the feature to compute distances for.
+
+        Returns:
+            distances (numpy array): Distances between the point and points.
+        """
+        distances = np.zeros(len(points))
+        for i, comparison_point in enumerate(points):
+            distances[i] = point_to_point_dist(point, comparison_point, feature_index)
+
+        return distances
+
+    def indices_to_points(X: np.ndarray, indices: np.ndarray) -> np.ndarray:
+        """
+        Get the points in the dataset corresponding to a given set of indices.
+
+        Parameters:
+            X (numpy array): Input data matrix (features).
+            indices (numpy array): Indices of the points to retrieve.
+
+        Returns:
+            points (numpy array): The points in the dataset corresponding to the given indices.
+        """
+        return X[indices]
+
     # Compute distances from the sample to its k-nearest neighbors
     neigh = NearestNeighbors(n_neighbors=k)
     neigh.fit(X)
-    _, neighb_indices = neigh.kneighbors(X[sample_idx].reshape(1, -1))
-    neighb_dist = np.mean(neigh.kneighbors()[0], axis=1)
 
+    sample_point = X[sample_idx]
+
+    neighb_indices = point_get_nearest_neighbors(neigh, sample_point)
+    neighb_points = indices_to_points(X, neighb_indices)
     # Compute distances from random points to their k-nearest neighbors
     rand_indices = np.random.choice(X.shape[0], size=num_random_points, replace=False)
-    rand_dist = np.zeros((num_random_points, X.shape[1]))
-    for i, rand_idx in enumerate(rand_indices):
-        log_trace(f"Random index: {rand_idx}")
-        _, rand_indices_neighb = neigh.kneighbors(X[rand_idx].reshape(1, -1))
-        rand_indices_neighb = rand_indices_neighb[0]
+    rand_points = indices_to_points(X, rand_indices)
 
-        rand_neigh_dists = []
-        for rand_neighb_idx in rand_indices_neighb:
-            dist = np.linalg.norm(X[rand_idx] - X[rand_neighb_idx])
-            rand_neigh_dists.append(dist)
-        log_trace(f"Random indices neighbors: {rand_indices_neighb} with distances: {rand_neigh_dists}")
-        rand_dist[i] = np.mean(rand_neigh_dists)
+    num_features = X.shape[1]
+    feature_importance_coefs = np.zeros(num_features)
+    for i, feature_index in enumerate(range(num_features)):
+        log_info(f"Computing feature importance for feature {feature_index}...")
 
-    # Compute feature importance as the ratio of neighb_dist to rand_dist
-    feature_importance = np.mean(neighb_dist, axis=0) / np.mean(rand_dist, axis=0)
-    log_debug(f"Feature importance: {feature_importance}")
+        neighb_dists = point_to_points_distances(X, sample_point, neighb_points, feature_index)
+        rand_dists = point_to_points_distances(X, sample_point, rand_points, feature_index)
 
-    return feature_importance
+        log_trace(f"Nearest neighbors distances: {neighb_dists}")
+        log_trace(f"Random neighbors distances: {rand_dists}")
+
+        neighb_dist_avg = np.mean(neighb_dists, axis=0)
+        rand_dist_avg = np.mean(rand_dists, axis=0)
+
+        log_debug(f"Nearest neighbors distances: {neighb_dist_avg}")
+        log_debug(f"Random neighbors distances: {rand_dist_avg}")
+
+        if neighb_dist_avg == 0 or rand_dist_avg == 0:
+            ratio = 0
+        else:
+            ratio = neighb_dist_avg / rand_dist_avg
+
+        log_debug(f"Feature importance: {ratio}")
+
+        feature_importance_coefs[i] = ratio
+
+    log_info(f"Feature importance coefficients: {feature_importance_coefs}")
+
+    return feature_importance_coefs
 
 
 def compute_average_feature_importance(X: np.ndarray, num_samples: int, k: int, num_random_points: int = 100) -> np.ndarray:
@@ -67,13 +157,16 @@ def compute_average_feature_importance(X: np.ndarray, num_samples: int, k: int, 
     log_debug(f"Selected sample indices: {sample_indices}")
 
     # Compute feature importance for each sample
-    feature_importance_sum = np.zeros(X.shape[1])
-    for sample_idx in sample_indices:
-        log_debug(f"Computing feature importance for sample {sample_idx}")
-        feature_importance_sum += compute_feature_importance(X, sample_idx, k, num_random_points)
+    num_features = X.shape[1]
+    feature_importances = np.zeros((num_samples, num_features))
+    for i, sample_idx in enumerate(sample_indices):
+        log_info(f"Computing feature importance for sample {sample_idx}. Progress: {i} / {num_samples}...")
+        feature_importances[i] = compute_feature_importance(X, sample_idx, k, num_random_points)
 
     # Average feature importance across samples
-    avg_feature_importance = feature_importance_sum / num_samples
+    log_debug(f"Feature importances: {feature_importances}")
+    avg_feature_importance = np.mean(feature_importances, axis=0)
+    log_info(f"Average feature importance: {avg_feature_importance}")
 
     return avg_feature_importance
 
