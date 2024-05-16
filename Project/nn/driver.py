e8a12ac278b0bcd88a780816c50cd539fbd244b7
Hugo Burton <burto732@gmail.com>
2024-05-16 21:33:01 +1000

    Increased epoch hyperparameter

--- Project/nn/driver.py
+++ Project/nn/driver.py
@@ -71,7 +71,7 @@ def run_nn_model(
     hidden_layer_dims = [100, 150, 100]
 
     # Hyperparameters
-    epochs = int(5e3)
+    epochs = int(2e4)
     batch_size = 1000
     learning_rate = 2e-4
     weight_decay = 0.01
