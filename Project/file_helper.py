e9f2a3155da54e618d484960980cbeb7d2e4cf42
Hugo Burton <burto732@gmail.com>
2024-05-18 15:40:04 +1000

    Adjusted log levels

--- Project/file_helper.py
+++ Project/file_helper.py
@@ -17,7 +17,7 @@ def make_folder_if_not_exists(folder_path: str) -> None:
 
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
-        log_info(f"Created folder: {folder_path}")
+        log_title(f"Created folder: {folder_path}")
     else:
         log_info(f"Folder already exists: {folder_path}")
 
@@ -48,11 +48,11 @@ def remove_file_if_exist(file_path: str, warn_if_not_exist: bool = False) -> Non
     # Check if the file exists
     if not file_exists(file_path):
         if warn_if_not_exist:
-            log_warning(f"File {file_path} does not exist")
+            log_error(f"File {file_path} does not exist")
         return
 
     os.remove(file_path)
-    log_info(f"Removed file: {file_path}")
+    log_warning(f"Removed file: {file_path}")
 
 
 def delete_folder_contents(folder_path: str) -> None:
