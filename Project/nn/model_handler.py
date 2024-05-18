9ba93cd945f4e29bbacfffdeed5bf2a15efc1cc4
Hugo Burton <burto732@gmail.com>
2024-05-18 11:50:28 +1000

    Initial commit on reading model

--- Project/nn/model_handler.py
+++ Project/nn/model_handler.py
@@ -5,13 +5,48 @@ Hugo Burton
 
 import os
 import torch
-from typing import List
+from typing import List, Union
+
+import file_helper
 
 from logger import *
 
 
-def read_model():
-    pass
+def read_model(model_save_path: str) -> Union[torch.nn.Module, None]:
+    """
+    Handler for reading a model from a file.
+
+    Args:
+    - model_save_path (str): The path to the model file.
+
+    Returns:
+    - torch.nn.Module: The model object if it exists else None.
+    """
+
+    # Check if the model file exists
+    if not file_helper.file_exists(model_save_path):
+        log_error(f"Model file does not exist: {model_save_path}")
+        return None
+
+    with open(model_save_path, "rb") as f:
+        model_obj = torch.load(f)
+
+    if not model_obj:
+        log_error(f"Model object is None")
+        return None
+
+    if not isinstance(model_obj, dict):
+        log_error(f"Model object is not a dictionary")
+        return None
+
+    # Check if the model is a checkpoint
+    if model_obj["is_checkpoint"]:
+        log_debug(
+            f"Reading checkpoint {model_obj['checkpoint_num']} from: {model_save_path}")
+    else:
+        log_debug(f"Reading final model from: {model_save_path}")
+
+    return model_obj
 
 
 def save_model(folder_path: str, model: torch.nn.Module, model_name: str, train_losses: List[float], validation_losses: List[float], checkpoint_num: int = None):
